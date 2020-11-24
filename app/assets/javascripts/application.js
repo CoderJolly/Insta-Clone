52299fb00ecadd05e3524a926453c45f6ca537d6
CoderJolly <harmanjolly97@gmail.com>
2020-11-24 06:41:19 +0530

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require rails-ujs
+//= require turbolinks
+//= require jquery3
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
+//= require will_paginate_infinite
